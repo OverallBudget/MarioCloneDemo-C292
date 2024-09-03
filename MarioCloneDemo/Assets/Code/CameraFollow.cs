7479662bcedd204dbc0495334b986d3a91aff96d
kamaharj <kamaharjan@hotmail.com>
2024-09-03 17:48:13 -0400

    Week2Tuesday
    Changes made from week2 Tuesday lab.

---
+++ MarioCloneDemo/Assets/Code/CameraFollow.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        follow();
+    }
+
+    private void follow()
+    {
+        Camera.current.transform.position = gameObject.transform.position;
+    }
+}
